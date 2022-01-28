2c71242e1e282c720dc97fe38ae62cf77a87028c
rhyou10 <rhyou10@nate.com>
2022-01-28 17:39:30 +0900

    blog_post 모델 생성 및 시간 설정

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
+from turtle import Turtle
 from django.db import models
 
+#게시물에 대한 모델
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    #author 추후 작성 에정
+
+    def __str__(self):
+        return f"[{self.pk}]{self.title}"
 # Create your models here.
