568ca42d631107cdd0830cdc8853c1fb3efdf45c
rhyou10 <rhyou10@nate.com>
2022-02-06 23:39:24 +0900

    author 필드 추가 및 foreignkey

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from distutils.command.upload import upload
 from turtle import Turtle
 from django.db import models
+from django.contrib.auth.models import User
 import os
 
 #게시물에 대한 모델
@@ -16,10 +17,11 @@ class Post(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    #author 추후 작성 에정
+    #author foeignkey로 만들어지고 User가 사라질때 model들도 같이 사라진다.
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"[{self.pk}]{self.title}"
+        return f"[{self.pk}]{self.title} :: {self.author}"
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
@@ -28,5 +30,5 @@ class Post(models.Model):
         return os.path.basename(self.file_upload.name)
 
     def get_file_ext(self):
-        return self.get_file_name().split('.')[-1]
+        return self.get_file_name().split('.')[-1] 
 # Create your models here.
