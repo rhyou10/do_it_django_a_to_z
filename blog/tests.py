940f31faab87220ecc3b5128d9f565792ac9f6d0
rhyou10 <rhyou10@nate.com>
2022-02-07 00:35:45 +0900

    포스트 목록, 상세페이지 작성자 추가

--- blog/tests.py
+++ blog/tests.py
@@ -1,6 +1,7 @@
 from urllib import response
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
+from django.contrib.auth.models import User
 from .models import Post
 
 # Create your tests here.
@@ -8,6 +9,8 @@ from .models import Post
 class TestVies(TestCase):
     def setUp(self):
         self.client = Client()
+        self.user_trump = User.objects.create_user(username='trump', password='somepassword')
+        self.user_obama = User.objects.create_user(username='obama', password='somepassword')
 
     def navbar_test(self, soup):
         navbar = soup.nav
@@ -31,13 +34,15 @@ class TestVies(TestCase):
     def test_post_list(self):
         #1.1 포스트 목록 페이지 가져오기
         response = self.client.get('/blog/')
+
         #1.2 정상적으로 페이지 로드된다.
         self.assertEqual(response.status_code, 200)
+
         #1.3 페이지 타이틀은 Blog인가
         soup = BeautifulSoup(response.content, 'html.parser')
         self.assertEqual(soup.title.text, 'Blog')
-        #1.4 내비게이션 바가 있다.
 
+        #1.4 내비게이션 바가 있다.
         #1.5 blog, aboutme 라는 문구가 내비게이션 바에 있다.
         self.navbar_test(soup)
 
@@ -51,10 +56,12 @@ class TestVies(TestCase):
         post_001 = Post.objects.create(
             title='1번',
             content = 'hello world',
+            author = self.user_trump,
         )
         post_002 = Post.objects.create(
             title = '2번',
             content = 'bye world',
+            author = self.user_obama
         )
         self.assertEqual(Post.objects.count(),2)
 
@@ -62,20 +69,25 @@ class TestVies(TestCase):
         response = self.client.get('/blog/')
         soup = BeautifulSoup(response.content, 'html.parser')
         self.assertEqual(response.status_code, 200)
+
         #3.3 메인 영역에 포스트 2개의 타이틀이 존재한다.
         main_area = soup.find('div', id='main-area')
         self.assertIn(post_001.title, main_area.text)
         self.assertIn(post_002.title, main_area.text)
+
         #3.4 아직 게시물이 없습니다. 라는 문구는 더이상 보이지 않는다.
         self.assertNotIn('아직 게시물이 없습니다.', main_area.text)
 
+        self.assertIn(self.user_trump.username.upper(), main_area.text.upper())
+        self.assertIn(self.user_obama.username.upper(), main_area.text.upper())
     def test_post_detail(self):
 
         #1.1 포스트가 하나있다.
 
         post_001 = Post.objects.create(
             title = '첫 포스트',
-            content = 'hello world'
+            content = 'hello world',
+            author = self.user_trump,
         )
 
         #1.2 그 포스트의 url은 '/blog/1/' 이다.
@@ -106,4 +118,7 @@ class TestVies(TestCase):
 
         #2.6 첫 번째 포스트 내용(content)이 포스트 영역에 있다.
         self.assertIn(post_001.content, post_area.text)
+
+
+        self.assertIn(self.user_trump.username.upper(), main_area.text.upper())
 
