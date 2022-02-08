3f0c0f9dad79499faf5607b665517040b2519c41
rhyou10 <rhyou10@nate.com>
2022-02-08 22:27:36 +0900

    포스트 상세페이지 카테고리 기능 추가

--- blog/tests.py
+++ blog/tests.py
@@ -132,20 +132,12 @@ class TestView(TestCase):
 
     def test_post_detail(self):
 
-        #1.1 포스트가 하나있다.
-
-        post_001 = Post.objects.create(
-            title = '첫 포스트',
-            content = 'hello world',
-            author = self.user_trump,
-        )
-
         #1.2 그 포스트의 url은 '/blog/1/' 이다.
-        self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
+        self.assertEqual(self.post_001.get_absolute_url(), '/blog/1/')
 
         #2. 첫 번째 포스트의 상세 페이지 테스트    
         #2.1 첫 번째 포스트의 url로 접근하면 정상적으로 작동한다 status code:200
-        response = self.client.get(post_001.get_absolute_url())
+        response = self.client.get(self.post_001.get_absolute_url())
         self.assertEqual(response.status_code, 200)
         
         #2.2 포스트 목록 페이지와 똑같은 내비게이션 바가 있다.
@@ -154,21 +146,24 @@ class TestView(TestCase):
         #self.assertIn('Blog', navbar.text)
         #self.assertIn('About Me', navbar.text)
         self.navbar_test(soup)
+        self.category_card_test(soup)
         
         #2.3 첫 번쨰 포스트의 제목이 웹 브라우저 탬 타이틀에 들어 있다.
-        self.assertIn(post_001.title, soup.title.text)
+        self.assertIn(self.post_001.title, soup.title.text)
         
         #2.4 첫 번째 포스트의 제목이 포스트 영역에 있다.
         main_area = soup.find('div', id='main-area')
         post_area = main_area.find('div', id='post-area')
-        self.assertIn(post_001.title, post_area.text )
+        self.assertIn(self.post_001.title, post_area.text )
 
         #2.5 첫 번째 포스트의 작성자(author)가 포스트 영역에 있다.(아직 구현할수 없음)
         
 
         #2.6 첫 번째 포스트 내용(content)이 포스트 영역에 있다.
-        self.assertIn(post_001.content, post_area.text)
+        self.assertIn(self.post_001.content, post_area.text)
+        self.assertIn(self.category_programming.name, post_area.text)
 
 
         self.assertIn(self.user_trump.username.upper(), main_area.text.upper())
+        self.assertIn(self.post_001.content, post_area.text)
 
