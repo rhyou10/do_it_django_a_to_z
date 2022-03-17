a210a4df292da1eea26ccb6f664ccf01509b694f
rhyou10 <rhyou10@nate.com>
2022-03-17 22:14:10 +0900

    사용자 아바타 생성 및 보여주기

--- blog/models.py
+++ blog/models.py
@@ -90,4 +90,11 @@ class Comment(models.Model):
     def get_absolute_url(self):
         return f"{self.post.get_absolute_url()}#comment-{self.pk}"
 
+    ## 아바다
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return f'https://doitdjango.com/avatar/id/699/39929cf293ce2cb4/svg/{self.author.email}'
+
 # Create your models here.
