15bf1370d27c603721f42d3508d58a25d6b86c39
rhyou10 <rhyou10@nate.com>
2022-03-18 19:28:08 +0900

    대문페이지 수정

--- blog/models.py
+++ blog/models.py
@@ -74,6 +74,12 @@ class Post(models.Model):
     
     def get_content_markdown(self):
         return markdown(self.content)
+    
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return f'https://doitdjango.com/avatar/id/699/39929cf293ce2cb4/svg/{self.author.email}'
 
 ## 댓글 모델
 
