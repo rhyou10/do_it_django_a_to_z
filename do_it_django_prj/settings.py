521495884d02a29fb697485496ae55270b0402af
rhyou10 <rhyou10@nate.com>
2022-03-19 20:55:36 +0900

    도커 시작

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -22,10 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!uqgp%tb#khchwe75%k5$j!(=c1=!1h!7l6*!#-)&012*y#2c+'
+SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-!uqgp%tb#khchwe75%k5$j!(=c1=!1h!7l6*!#-)&012*y#2c+')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', 1))
+
+if os.environ.get('DJANGO_ALLOWED_HOSTS'):
+    ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
+else:
+    ALLOWED_HOSTS = []
 
 ALLOWED_HOSTS = []
 
